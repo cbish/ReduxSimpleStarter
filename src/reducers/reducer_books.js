5c31d8e531115706a457b3a28f9489f72361869c
cbish <chris.bishop1337@gamil.com>
2018-01-16 12:06:52 -0700

    project 2 books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Book 1', pages: 111},
+        {title: 'Book 2', pages: 222},
+        {title: 'Book 3', pages: 333},
+        {title: 'Book 4', pages: 444}
+    ];
+}
