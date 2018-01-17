e5bc56a59201168ce08a1e9e75b9b10ee63ffc41
cbish <chris.bishop1337@gamil.com>
2018-01-17 13:06:32 -0700

    form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
+        return(
+            <div className={className}>
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
+            </div>
+        );
+    }
+
+    onSubmit(values) {
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <div></div>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <button className="btn btn-primary">Cancel</button>
+            </form>
         );
     }
 }
 
-export default PostsNew;
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = "Enter a title.";
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
